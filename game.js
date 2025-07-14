3c30d819d98524457fc7547c245126b659cd6dd5
Pablo Alba <pablo.alba@kaleidos.net>
2025-07-14 12:04:44 +0200

    editor 0.1 working

--- game.js
+++ game.js
@@ -4,6 +4,7 @@ const TILE_SIZE = 128
 const TILE_OFFSET_X = 256
 const TILE_OFFSET_Y = 128
 const MAX_FRAMES = 30
+const MAX_CHARACTERS = 2
 
 const TILE_OFFSET_EDITOR = 4
 
@@ -17,25 +18,14 @@ const tiles = []
 let animTime = 0
 let lastTime = 0
 let currentFrame = 0
-let editorSelected = 0
 let editing = false
 let buttons = []
 let characters = []
+let targets = []
 let selectedButton = null
 let lastId = -1
+let currentLevel = 0
 
-
-const warrior = {
-    name: "warrior",
-    x: 0,
-    y: 0,
-    targetX: 0,
-    targetY: 0,
-    currentAnim: "idle",
-    anims: {
-        "idle": []
-    }
-}
 
 
 var board = [
@@ -49,18 +39,52 @@ var board = [
 
 
 async function preloadTiles() {
-    for (var i = 0; i < 15; i++) {
+    for (var i = 0; i < 17; i++) {
         const img = new Image();
         img.src = "img/tiles/" + String(i).padStart(3, '0') + ".png"
         tiles.push(img);
     }
 }
 
-async function preloadCharacter(character) {
+async function preloadCharacter(num) {
+    var character = {
+        num: num,
+        enabled: false,
+        x: 0,
+        y: 0,
+        exitX: 0,
+        exitY: 0,
+        currentAnim: "idle",
+        anims: {
+            "idle": []
+        },
+        img: {
+            base: null,
+            exit: null
+        }
+    }
+
+    var img
     for (var i = 0; i < MAX_FRAMES; i++) {
-        const img = new Image();
-        img.src = "img/characters/" + character.name + "/idle/" + String(i).padStart(3, '0') + ".png"
+        img = new Image();
+        img.src = "img/characters/" + num + "/idle/" + String(i).padStart(3, '0') + ".png"
         character.anims.idle.push(img);
+    }
+
+    img = new Image();
+    img.src = "img/characters/" + num + "/base.png"
+    character.img.base = img
+
+    img = new Image();
+    img.src = "img/characters/" + num + "/exit.png"
+    character.img.exit = img
+    return character
+}
+
+async function preloadCharacters() {
+    for (var i = 0; i < MAX_CHARACTERS; i++) {
+        var character = await preloadCharacter(i)
+        characters.push(character)
     }
 }
 
@@ -70,9 +94,9 @@ function generateId() {
     return lastId
 }
 
-function addButton(img, x, y, width, height, value, onClick) {
+function addButton(img, x, y, width, height, value, type, onClick) {
     buttons.push(
-        { id: generateId(), img: img, x: x, y: y, width: width, height: height, value: value, onClick: onClick }
+        { id: generateId(), img: img, x: x, y: y, width: width, height: height, value: value, type: type, onClick: onClick }
     )
 }
 
@@ -122,11 +146,18 @@ function pointInRect(px, py, rx, ry, rw, rh) {
 }
 
 
-function drawTile(x, y, tileNum, border) {
+function drawTile(x, y, tileNum,) {
     x = TILE_OFFSET_X + (x * TILE_SIZE)
     y = TILE_OFFSET_Y + (y * TILE_SIZE)
 
     ctx.drawImage(tiles[tileNum], x, y);
+}
+
+function drawExit(x, y, targetNum) {
+    x = TILE_OFFSET_X + (x * TILE_SIZE)
+    y = TILE_OFFSET_Y + (y * TILE_SIZE)
+
+    ctx.drawImage(target[targetNum], x, y);
 }
 
 function drawBoard(delta) {
@@ -139,8 +170,10 @@ function drawBoard(delta) {
 
 
 function drawCharacter(character, delta) {
-
-    ctx.drawImage(character.anims[character.currentAnim][currentFrame], character.x, character.y);
+    if (character.enabled) {
+        ctx.drawImage(character.img.exit, character.exitX * TILE_SIZE + TILE_OFFSET_X, character.exitY * TILE_SIZE + TILE_OFFSET_Y);
+        ctx.drawImage(character.anims[character.currentAnim][currentFrame], character.x * TILE_SIZE + TILE_OFFSET_X, character.y * TILE_SIZE + TILE_OFFSET_Y);
+    }
 }
 
 function drawCharacters(delta) {
@@ -211,48 +244,98 @@ function displayGameArea() {
 function initializeBoard(onClick) {
     for (var y = 0; y < 6; y++) {
         for (var x = 0; x < 6; x++) {
-            addButton(null, x * TILE_SIZE, y * TILE_SIZE, TILE_SIZE, TILE_SIZE, null, onClick)
+            addButton(null, x * TILE_SIZE + TILE_OFFSET_X, y * TILE_SIZE + TILE_OFFSET_Y, TILE_SIZE, TILE_SIZE, null, "tile", onClick)
         }
     }
 }
 
 
 function startGame() {
-    characters = []
     buttons = []
+    targets = []
     loadMap(0)
 
     displayGameArea()
 }
 
 
+function logLevel() {
+
+    levelTxt = "{\n"
+    levelTxt += "  map: [" + board.map(row => row.join(', ')).join(', ') + "],\n"
+    levelTxt += "  characters: [\n"
+    characters.forEach(character => {
+        levelTxt += "    {\n"
+        levelTxt += "    num: " + character.num + ",\n"
+        levelTxt += "    enabled: " + character.enabled + ",\n"
+        levelTxt += "    x: " + character.x + ",\n"
+        levelTxt += "    y: " + character.y + ",\n"
+        levelTxt += "    exitX: " + character.exitX + ",\n"
+        levelTxt += "    exitY: " + character.exitY + ",\n"
+        levelTxt += "    },\n"
+    })
+    levelTxt += "  ]\n"
+    levelTxt += "}\n"
+
+    console.log(levelTxt)
+}
 
 function editMode() {
-    characters = []
+    loadLevel(LEVELS[currentLevel]);
+
     buttons = []
+    targets = []
 
     let onClick = (button) => {
+        console.log("onClick", button);
         selectedButton = button;
+        logLevel()
     }
 
 
     let x = -10
-    let y = 350
-    for (i = 0; i < 15; i++) {
+    let y = 128
+    for (i = 0; i < 17; i++) {
         x += 60
         if (x > 180) {
             x = 50
             y += 60
         }
-
-        addButton(tiles[i], x, y, 50, 50, i, onClick)
+        addButton(tiles[i], x, y, 50, 50, i, "editTile", onClick)
     }
+
+
+
+    y += 120
+
+    characters.forEach(character => {
+        addButton(character.img.base, 50, y, 50, 50, character.num, "editCharacter", onClick)
+        addButton(character.img.exit, 110, y, 50, 50, character.num, "editExit", onClick)
+        y += 60
+    })
+
 
 
     let onBoardClick = (button) => {
         var [bx, by] = coordsToTile(button.x + 1, button.y + 1)
-        console.log("bxby", bx, by)
-        board[by][bx] = selectedButton.value
+
+        if (selectedButton.type == "editTile") {
+            board[by][bx] = selectedButton.value
+        } else if (selectedButton.type == "editCharacter") {
+            if (characters[selectedButton.value].enabled && characters[selectedButton.value].x == bx && characters[selectedButton.value].y == by) {
+                characters[selectedButton.value].enabled = false
+            } else {
+                characters[selectedButton.value].enabled = true
+                characters[selectedButton.value].x = bx
+                characters[selectedButton.value].y = by
+            }
+        } else if (selectedButton.type == "editExit") {
+            characters[selectedButton.value].exitX = bx
+            characters[selectedButton.value].exitY = by
+        }
+
+
+        logLevel()
     }
 
 
@@ -260,16 +343,32 @@ function editMode() {
 
 
     displayGameArea()
+
+    buttons[0].onClick(buttons[0])
 }
 
-function loadMap(mapNum) {
-    characters = [warrior]
-    setCharacterPos(warrior, 0, 0);
+function loadLevel(level) {
+    let i = 0;
+    for (var y = 0; y < 6; y++) {
+        for (var x = 0; x < 6; x++) {
+            board[y][x] = level.map[i]
+            i++
+        }
+    }
+
+    for (i = 0; i < characters.length; i++) {
+        characters[i].enabled = level.characters[i].enabled
+        characters[i].x = level.characters[i].x
+        characters[i].y = level.characters[i].y
+        characters[i].exitX = level.characters[i].exitX
+        characters[i].exitY = level.characters[i].exitY
+    }
+
 }
 
 function initialize() {
     preloadTiles();
-    preloadCharacter(warrior);
+    preloadCharacters();
 
     resizeCanvas();
 
---
+++ levels.js
@@ -0,0 +1,23 @@
+const LEVELS = [
+    {
+        map: [7, 1, 1, 1, 1, 5, 4, 0, 0, 0, 0, 2, 4, 0, 7, 0, 0, 2, 4, 0, 0, 0, 0, 2, 4, 0, 0, 0, 0, 2, 10, 3, 3, 3, 3, 8],
+        characters: [
+            {
+                num: 0,
+                enabled: true,
+                x: 2,
+                y: 2,
+                exitX: 4,
+                exitY: 2,
+            },
+            {
+                num: 1,
+                enabled: true,
+                x: 4,
+                y: 4,
+                exitX: 1,
+                exitY: 5,
+            },
+        ]
+    }
+]
